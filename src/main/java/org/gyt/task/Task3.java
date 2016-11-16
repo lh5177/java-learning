a8f003f5fcf73918e5b340c22dacee928030b806
AaricChen <chopinchen@vip.sina.com>
2016-11-16 15:54:44 +0800

    增加下一次作业

---
+++ src/main/java/org/gyt/task/Task3.java
@@ -0,0 +1,11 @@
+package org.gyt.task;
+
+/**
+ * 作业三
+ * 返回一个从1到100组成的字符串，格式如下：
+ * 1,2,3,4,...,97,98,99,100
+ * Created by y27chen on 2016/11/16.
+ */
+public interface Task3 extends Task {
+    String getValues();
+}
---
+++ src/main/java/org/gyt/task/Task4.java
@@ -0,0 +1,14 @@
+package org.gyt.task;
+
+/**
+ * 作业四
+ * 返回斐波那契数列前n项的字符串，n通过参数传递，格式如下
+ * n=1: 1
+ * n=2: 1,1
+ * n=3: 1,1,2
+ * n=5: 1,1,2,3,5
+ * n=8: 1,1,2,3,5,8,13,21
+ * Created by y27chen on 2016/11/16.
+ */
+public interface Task4 extends Task {
+}
