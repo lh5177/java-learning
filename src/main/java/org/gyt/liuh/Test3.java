682f55383858537eeb1c229a4a0b5170afa03906
Administrator <172200573@qq.com>
2016-11-29 21:37:23 +0800

    作业3

--- src/main/java/org/gyt/liuh/Test3.java
+++ src/main/java/org/gyt/liuh/Test3.java
@@ -6,10 +6,5 @@ import org.gyt.liuh.impl.Practice3Impl;
  * Created by 刘洪 on 2016-11-25.
  */
 public class Test3 extends Practice3Impl{
-    public static void main(String[] args) {
-        Test3 pra =new Test3();
-        System.out.println(pra.getValues(10));
-        System.out.println(pra.getValues(20));
 
-    }
 }
